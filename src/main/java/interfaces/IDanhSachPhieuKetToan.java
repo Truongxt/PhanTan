480ec337701d7c454f86ddb30ad48e025c3c3ef3
Truongxt <132972217+Truongxt@users.noreply.github.com>
2025-04-20 18:55:43 +0700

    Update IDanhSachPhieuKetToan.java

--- src/main/java/interfaces/IDanhSachPhieuKetToan.java
+++ src/main/java/interfaces/IDanhSachPhieuKetToan.java
@@ -9,15 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IDanhSachPhieuKetToan extends Remote {
-<<<<<<< HEAD
-    KetToan getOne(String maKetToan);
-
-    ArrayList<KetToan> getAll();
-
-    ArrayList<KetToan> getByDate(Date start, Date end);
-=======
     KetToan getOne(String maKetToan) throws Exception;
      ArrayList<KetToan> getAll()throws Exception;
     ArrayList<KetToan> getByDate(Date start, Date end) throws Exception;
->>>>>>> 958cd70d04bbf44f3c05477c0e0cf8618cd0f04b
 }
